367c2fba2a8aac66c1039978c55aac6555bc607a
sun1devil <codejmc11@gmail.com>
2018-05-21 17:08:17 -0700

    connect with twitter and Spotify working, create seperate functions

--- liri.js
+++ liri.js
@@ -1,12 +1,13 @@
-// require("dotenv").config();
 
-
-
+require("dotenv").config();
 var fs = require("fs");
 var request = require("request");
 var inquirer = require("inquirer");
-
-
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var keys = require("./keys.js")
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 
 // {
@@ -31,70 +32,98 @@ var inquirer = require("inquirer");
 
 // // * `movie-this`
 
-// inquirer.prompt(
-//     {
-//         type: "input",
-//         name: "usermovie",
-//         message: "What is your favorite movie?",
+inquirer.prompt(
+    {
+        type: "list",
+        name: "questions",
+        message: "What do you want to do?",
+        choices: [ "Movie", "Tweets", "Songs"]
+    }).then (function (response){
+        console.log(response.questions)
+        if(response.questions === "Movie"){
+            runMovie()
+        }
+        else if (response.questions === "Tweets"){
+            runTweets()
+        }else if (response.questions === "Songs"){
+            runSpotify()
+        }
+        
+    }) 
 
 
-//     }).then(function (response){
-//         // console.log(response);
-//         // console.log(response.usermovie);
-//         // var movieArgs = response.usermovie;
-//         var movieName = (response.usermovie);
-//         console.log(movieName);
-//         var omdbQueryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-//         console.log(omdbQueryUrl);
+function runMovie (){
 
-//         request(omdbQueryUrl, function(error, response, body) {
-
-// //   // If the request is successful
-//   if (!error && response.statusCode === 200) {
-
-// //     // Parse the body for data
-//         console.log("Movie Title: " + JSON.parse(body).Title);
-//     console.log("Release Year: " + JSON.parse(body).Year);
-//     console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-//     console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-//     console.log("Produced In: " + JSON.parse(body).Country);
-//     console.log("Language: " + JSON.parse(body).Language);
-//     console.log("Plot: " + JSON.parse(body).Plot);
-//     console.log("Actors: " + JSON.parse(body).Actors);
-//                   } 
-//                 });
-//             });
+inquirer.prompt(
+    {
+        type: "input",
+        name: "usermovie",
+        message: "What is your favorite movie?",
 
 
+    }).then(function (response){
+        // console.log(response);
+        // console.log(response.usermovie);
+        // var movieArgs = response.usermovie;
+        var movieName = (response.usermovie);
+        console.log(movieName);
+        var omdbQueryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        console.log(omdbQueryUrl);
+
+        request(omdbQueryUrl, function(error, response, body) {
+
+//   // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+//     // Parse the body for data
+        console.log("Movie Title: " + JSON.parse(body).Title);
+    console.log("Release Year: " + JSON.parse(body).Year);
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+    console.log("Produced In: " + JSON.parse(body).Country);
+    console.log("Language: " + JSON.parse(body).Language);
+    console.log("Plot: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
+                  } 
+                });
+            });
+
+        }
 
 // // // * `spotify-this-song`
 
-
-// inquirer.prompt(
-// {
-//     type: "input",
-//     name: "usersong",
-//     message: "What is your favorite song?",
-
-
-// }).then(function (song){
-//       console.log(song);
-//     console.log(song.usersong);
-//     var songName = (song.usersong);
-//     console.log(songName);
-// });
+function runSpotify () { 
 
 
 
-// fs.readFile("random.txt", "utf8", function(error, data) {
+inquirer.prompt(
+    {
+        type: "input",
+        name: "usersong",
+        message: "What is your favorite song?",
 
 
-// var spotifyQueryUrl = "http://open.spotify.com/track/search?q=track:I%20need%20a%20hero/json/20e2b8e631c041ac8391365a7e8fea37" 
-// // + spotifyName + "c456b10c71046b09b127295af165d61";  
-// console.log(spotifyQueryUrl);
+    }).then(function (song) {
+        // console.log(song);
+        // console.log(song.usersong);
+        var songName = (song.usersong);
+        // console.log(songName);
+        spotify.search({ type: 'track', query: songName }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
 
+            // console.log(data);
+            var currSong = data.tracks.items[0];
+            // console.log(currSong);
+            console.log(currSong.artists[0].name);
+            console.log(currSong.album.name);
+            console.log(currSong.album.external_urls.spotify)
+            console.log(currSong.name)
+        });
+    });
+}
 
-// request(spotifyQueryUrl, function(error, response, body) {
 
 //   // If the request is successful
 //   if (!error && response.statusCode === 200) {
@@ -116,6 +145,8 @@ var inquirer = require("inquirer");
 
 // //  * `my-tweets`
 
+function runTweets() {
+
 inquirer.prompt(
     {
         type: "confirm",
@@ -128,23 +159,40 @@ inquirer.prompt(
         if (choice.tweetchoice) {
             console.log("OK, I'll show you them");
 
-            var twitterQueryUrl = "https://api.twitter.com/1.1/CODEJMC11/search/tweets/JSON/997983032626696194-CXeR9YNibO69asQrPpJXPZJZUBROnbc";  
+            var params = {screen_name: 'codejmc11'};
 
-             console.log(twitterQueryUrl);
-            
-            
-            //  request(twitterQueryUrl, function(error, response, body) {
-            
-            // // //    // If the request is successful
-            // //    if (!error && response.statusCode === 200) {
-            // //      console.log(JSON);
-            // // // // //      // Parse the body for data
-            // // // // //      console.log("TWEETS: " + JSON.parse(body).XXXX);
-        } else {
-            console.log("I'm offended, Go Away!")
-        
-        };
-    });
+            client.get('statuses/user_timeline', params, function(error, tweets, response) {
+              if (!error) {
+                // console.log(tweets);
+                for (i =0; i < tweets.length; i++){
+                    console.log(tweets[i].created_at)
+                    console.log(tweets[i].text)
+                }
+
+              }
+                          });
+        }
+    })
+}
+
+
+
+
+
+
+
+    //          request(twitterQueryUrl, function(error, response, body) {
+
+    //         // //    // If the request is successful
+    //            if (!error && response.statusCode === 200) {
+    //              console.log(JSON);
+    //         // // // // //      // Parse the body for data
+    //         // // // // //      console.log("TWEETS: " + JSON.parse(body).XXXX);
+    //     } else {
+    //         console.log("I'm offended, Go Away!")
+
+    //     };
+    // });
 
 
 
